b2814f5bf33a148c527f56cc9b2fd129c07847fe
BlvckAres <xzchauke@proton.me>
2022-09-23 02:57:58 +0000

    uploaded functions and did some adjustments

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
+from calculator_1 import add, sub, mul, div
+    def main()
 
     a = 10
     b = 5
@@ -10,3 +10,6 @@ if __name__ == "__main__":
     print("{:d} - {:d) = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
     print("{:d} / {:d} = {:d}".format(a, b,(round(div(a, b))))
+
+if __name__ == "__main__":
+    main()
