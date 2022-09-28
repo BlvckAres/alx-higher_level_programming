66bbf60b1fb6703533894049227a25214ed82d1a
BlvckAres <xzchauke@proton.me>
2022-09-28 00:24:11 +0000

    uploaded function

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def no_c(my_string):
     char_to_remove = ["C","c"]
-    y = ""
+    new_string = ""
     for char in my_string:
         if char not in char_to_remove:
-            y += char
-    return y
+            new_string += char
+    return new_string
