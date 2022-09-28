57d7a91de0256246eb7e36ee73d9d5d876b1b5c5
BlvckAres <xzchauke@proton.me>
2022-09-28 00:35:20 +0000

    adjusted function

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -4,7 +4,7 @@ def divisible_by_2(my_list=[]):
     list_result = []
     for x in my_list:
         if x % 2 == 0:
-            list_result.append("True")
+            list_result.append(True)
         elif x % 2 != 0:
-            list_result.append("False")
+            list_result.append(False)
     return list_result
