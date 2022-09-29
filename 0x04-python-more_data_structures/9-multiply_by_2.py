3e07f6f47aa6f012cc250c69309d0d7b31cbb36a
BlvckAres <xzchauke@proton.me>
2022-09-29 00:54:43 +0000

    adjusted function

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = {}
-    for x in a_dictionary:
-      new_dict[x] = a_dictionary[x] * 2
-    return new_dict
+    new_dict = a_dictionary.copy()
+    for x in new_dict.keys():
+      new_dict[x] *= 2
+    return (new_dict)
