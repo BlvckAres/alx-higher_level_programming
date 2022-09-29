1276e56d8ed6359dd928ea56ddefdadb81bf87bd
BlvckAres <xzchauke@proton.me>
2022-09-29 01:08:15 +0000

    uploaded task_10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,10 +3,10 @@ def best_score(a_dictionary):
     if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
         return None
 
-    ret = list(a_dictionary.keys())[0]
-    big = a_dictionary[ret]
+    reslt = list(a_dictionary.keys())[0]
+    best = a_dictionary[reslt]
     for k, v in a_dictionary.items():
         if v > big:
-            big = v
-            ret = k
-    return (ret)
+            best = v
+            reslt = k
+    return (reslt)
