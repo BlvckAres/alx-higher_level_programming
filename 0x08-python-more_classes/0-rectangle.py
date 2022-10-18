bbef7b13a616d80f6cae58e4ec727b829b319281
BlvckAres <xzchauke@proton.me>
2022-10-18 03:20:32 +0000

    uploaded functions.

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""A class that defines a rectangle"""
+
+
+class Rectangle:
+    """Represents an empty rectangle class"
+    pass	
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+In this project we look at more python classes and objects.
