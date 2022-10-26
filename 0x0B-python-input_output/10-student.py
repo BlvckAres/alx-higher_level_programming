50f0c5bb3a388a63b30f86a592a6c7746cbebaa8
BlvckAres <xzchauke@proton.me>
2022-10-26 14:39:11 +0000

    adjusted function.

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -6,13 +6,15 @@ class Student:
     """Defining the Student class."""
 
     def __init__(self, first_name, last_name, age):
-    """Initialize a new student."""
+    """Initialize a new student.
+    """
         self.first_name = first_name
 	self.last_name = last_name
 	self.age = age
 
     def to_json(self, attrs=None):
-        """ Getting a dict representation of the student."""
+        """ Getting a dict representation of the student.
+        """
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
