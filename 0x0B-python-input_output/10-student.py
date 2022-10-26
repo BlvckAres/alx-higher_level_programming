0a2c0ee56e6c1a693c1505929c35ce11b09d56ad
BlvckAres <xzchauke@proton.me>
2022-10-26 14:41:35 +0000

    adjusted function.

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-"""A class Student that defines a student."""
+"""This module defines a class Student"""
 
 
 class Student:
-    """Defining the Student class."""
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-    """Initialize a new student.
-    """
+        """Initializes a new Student
+        """
         self.first_name = first_name
-	self.last_name = last_name
-	self.age = age
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self, attrs=None):
-        """ Getting a dict representation of the student.
+        """Get a dictionary representation of the Student
+        If attrs is a list of strings, represents only those attributes
+        included in the list
         """
         if (type(attrs) == list and
                 all(type(ele) == str for ele in attrs)):
