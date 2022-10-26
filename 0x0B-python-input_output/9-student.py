4b92aede9f0f334720feec217820283b8116a150
BlvckAres <xzchauke@proton.me>
2022-10-26 14:28:34 +0000

    adjusted functions.

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,16 +3,17 @@
 
 
 class Student:
-    """ Defining a class name Student.
-
-        Args:
-        first_name(str): The first name
-        last_name(str): The last name
-        age(int): The age
-    """
+    """ Defining a class name Student."""
 
     def __init__(self, first_name, last_name, age):
-        """ Initialize a new student. """
+        """ Initialize a new student.
+
+	 Args:
+            first_name(str): The first name
+            last_name(str): The last name
+            age(int): The age
+        """
+
         self.first_name = first_name
 	self.last_name = last_name
 	self.age = age
