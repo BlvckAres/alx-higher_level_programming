0a89f42919d4410e8a0e7d48160abcf522203aa0
BlvckAres <xzchauke@proton.me>
2022-10-26 14:33:35 +0000

    adjusted functions.

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -3,21 +3,15 @@
 
 
 class Student:
-    """ Defining a class name Student."""
+    """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """ Initialize a new student.
-
-	 Args:
-            first_name(str): The first name
-            last_name(str): The last name
-            age(int): The age
+        """Initializes a new Student
         """
-
         self.first_name = first_name
-	self.last_name = last_name
-	self.age = age
+        self.last_name = last_name
+        self.age = age
 
     def to_json(self):
-        """ Returns a dictionary of the student."""
+        """Gets a dictionary representation of the Student"""
         return self.__dict__
