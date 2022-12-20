c9050563e6302e2b1aeded1575463088408557d9
BlvkcAres <xzchauke@protonmail.me>
2022-12-20 05:42:47 +0200

    uploaded javascript functions

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const size = Math.floor(Number(process.argv[2]));
+if (isNaN(size)) {
+    console.log('Missing size');}
+else {
+    for (let y = 0; y < size; y++) {
+        let row = '';
+        for (let s = 0; s < size; s++) row += "X";
+        console.log(row);
+    }
+}
